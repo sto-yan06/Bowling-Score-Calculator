8ad3a48780124c3db3ed58287af9a931a3402359
George <stoian.george06@gmail.com>
2023-05-17 13:53:55 +0300

    Updated with Tests

--- main.py
+++ main.py
@@ -1,50 +1,94 @@
-frames_scores = []
-current_frame = 1
-current_roll = 1
-total_score = 0
+import unittest
+from unittest.mock import patch
+import builtins
 
-while current_frame <= 10:
 
-    pins = int(input("Enter the number of pins knocked down on roll " + str(current_roll) + " of frame " + str(current_frame) + ": "))
+class BowlingGameTests(unittest.TestCase):
+    def test_with_given_example(self):
+        with patch('builtins.input',
+                   side_effect=['1', '4', '4', '5', '6', '4', '5', '5', '5', '10', '10', '0', '1', '0', '1',
+                                '7', '3', '6', '6', '4', '10', '10', '2', '8', '2', '8', '6']):
+            expected_score = int(133)
+            actual_score = self.play_game()
+            self.assertEqual(actual_score, expected_score)
 
-    total_score += pins
+    def test_with_all_strikes(self):
+        with patch('builtins.input', side_effect=['10'] * 20):
+            expected_score = int(300)
+            actual_score = self.play_game()
+            self.assertEqual(actual_score ,expected_score)
 
-    if current_roll == 2 or pins == 10:
+    def tests_with_all_zero(self):
+        with patch('builtins.input', side_effect=['0'] * 20):
+            expected_score = int(0)
+            actual_score = self.play_game()
+            self.assertEqual(expected_score, actual_score)
 
-        if current_roll == 1 and pins == 10:
-            next_pins_strike_1 = int(
-                input("You hit a Strike, Enter the number of pins knocked down on the first roll: "))
-            total_score += next_pins_strike_1
-            if next_pins_strike_1 == 10:
-                total_score += 10
-            else:
-                next_pins_strike_2 = int(
-                    input("You hit a Strike, Enter the number of pins knocked down on the second roll: "))
-                total_score += next_pins_strike_2
+    def tests_with_all_spares(self):
+        with patch('builtins.input', side_effect=['5','5','5'] * 30):
+            expected_score = int(150)
+            actual_score = self.play_game()
+            self.assertEqual(expected_score, actual_score)
 
-        if current_roll == 1 and len(frames_scores) > 0 and frames_scores[-1] == 10:
-            total_score += pins
-            if current_frame == 10 and pins == 10:
-                bonus_pins_1 = int(input("You hit a Strike on tenth one! Roll two more: "))
-                total_score += bonus_pins_1
-                bonus_pins_2 = int(input("You hit a Strike on tenth frame! Roll one more: "))
-                total_score += bonus_pins_2
+    def test_with_random_numbers(self):
+        with patch('builtins.input', side_effect=['3','5','5','5','5','5','4','5','5','10','10','2','6','2','6','8','1','3','5','10','2','4','2','4']):
+            expected_score = int(117)
+            actual_score = self.play_game()
+            self.assertEqual(expected_score, actual_score)
 
-        elif current_roll == 2 and total_score == 10:
-            if current_frame < 10:
-                next_pins_spare = int(input("You hit a Spare, Enter the number of pins knocked down on the next roll: "))
-                total_score += next_pins_spare
-            else:
-                bonus_pins = int(input("You hit a Spare on tenth frame! Roll one more: "))
-                total_score += bonus_pins
-        frames_scores.append(total_score)
-        print("Score after frame " + str(current_frame) + ": " + str(sum(frames_scores)))
-        total_score = 0
-        current_frame += 1
+
+    def play_game(self):
+
+        frames_scores = []
+        current_frame = 1
         current_roll = 1
+        total_score = 0
+        while current_frame <= 10:
 
-    else:
-        current_roll += 1
+            pins = int(input("Enter the number of pins knocked down on roll " + str(current_roll) + " of frame " + str(current_frame) + ": "))
+
+            total_score += pins
+
+            if current_roll == 2 or pins == 10:
+
+                if current_roll == 1 and pins == 10:
+                    next_pins_strike_1 = int(
+                        input("You hit a Strike, Enter the number of pins knocked down on the first roll: "))
+                    total_score += next_pins_strike_1
+                    if next_pins_strike_1 == 10:
+                        total_score += 10
+                    else:
+                        next_pins_strike_2 = int(
+                            input("You hit a Strike, Enter the number of pins knocked down on the second roll: "))
+                        total_score += next_pins_strike_2
+
+                if current_roll == 1 and len(frames_scores) > 0 and frames_scores[-1] == 10:
+                    total_score += pins
+                    if current_frame == 10 and pins == 10:
+                        bonus_pins_1 = int(input("You hit a Strike on tenth one! Roll two more: "))
+                        total_score += bonus_pins_1
+                        bonus_pins_2 = int(input("You hit a Strike on tenth frame! Roll one more: "))
+                        total_score += bonus_pins_2
+
+                elif current_roll == 2 and total_score == 10:
+                    if current_frame < 10:
+                        next_pins_spare = int(input("You hit a Spare, Enter the number of pins knocked down on the next roll: "))
+                        total_score += next_pins_spare
+                    else:
+                        bonus_pins = int(input("You hit a Spare on tenth frame! Roll one more: "))
+                        total_score += bonus_pins
+                frames_scores.append(total_score)
+                print("Score after frame " + str(current_frame) + ": " + str(sum(frames_scores)))
+                total_score = 0
+                current_frame += 1
+                current_roll = 1
+
+            else:
+                current_roll += 1
 
 
-print("Final score: " + str(sum(frames_scores)))
+        print("Final score: " + str(sum(frames_scores)))
+        return int(sum(frames_scores))
+
+
+
